33478b23e2c4972ba38e92ebf7747210f201e8da
parthibankrpa <parthibankrpa@gmail.com>
2019-01-25 07:25:35 -0500

    python learning

---
+++ ex9.py
@@ -0,0 +1,15 @@
+# Here's some new strange stuff, remember type it exactlyself.
+
+days =  "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days : ", days)
+print("Here are the months : ",months)
+
+
+print("""
+There's something going on hereself.
+with the three double-qoute4s
+We'll be able to type as much we likeself.
+Even 4 lines if we want, or 5, or 6,
+""")
